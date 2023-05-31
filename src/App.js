eda9e29ac2c2fdd7d591221fb322179c824a6aae
AyushGurg <120599569+AyushGurg@users.noreply.github.com>
2023-05-30 19:13:22 -0600

    learning

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import uniqid from "uniqid";
+import Overview from "./Overview";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      count: 0,
+      task: {
+        text: "",
+        id: uniqid(),
+      },
+      tasks: [],
     };
-
-    this.countUp = this.countUp.bind(this);
   }
 
-  countUp() {
+  handleChange = (e) => {
     this.setState({
-      count: this.state.count + 1,
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
     });
-  }
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: "",
+        id: uniqid(),
+      },
+    });
+  };
 
   render() {
+    const { task, tasks } = this.state;
+
     return (
       <div>
-        <button onClick={this.countUp}>Click Me!</button>
-        <p>{this.state.count}</p>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
+}
+
+export default App;
